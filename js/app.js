ece0bac074734913d1728829944efda7af2e7af7
doppl3r <deben3@gmail.com>
2019-12-17 14:53:03 -0700

    add antialiasing

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ var scene = new THREE.Scene();
 var camera = new THREE.PerspectiveCamera( 75, window.innerWidth/window.innerHeight, 0.1, 1000 );
 scene.background = new THREE.Color(0xf8d4de);
 
-var renderer = new THREE.WebGLRenderer();
+var renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
