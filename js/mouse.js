8479e11605e956a84d8e8ae384f0cdde96dcd61d
doppl3r <deben3@gmail.com>
2020-05-09 15:39:11 -0700

    add drag difference function

--- js/mouse.js
+++ js/mouse.js
@@ -3,17 +3,29 @@ class Mouse {
         this.down = new THREE.Vector3();
         this.move = new THREE.Vector3();
         this.up = new THREE.Vector3();
+        this.drag = false;
     }
     
     mouseDown = function(position) {
         this.down = position;
+        this.move = position; // Reset move
+        this.drag = true;
     }
 
     mouseMove = function(position) {
-        this.move = position;
+        if (this.drag == true) this.move = position;
     }
 
     mouseUp = function(position) {
         this.up = position;
+        this.drag = false;
+    }
+
+    getDragDifference = function() {
+        return { 
+            x: this.down.x - this.move.x, 
+            y: this.down.y - this.move.y, 
+            z: this.down.z - this.move.z
+        }
     }
 }
