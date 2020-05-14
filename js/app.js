02a4308806edec3996ff487e4c01942cd9ea7724
Jacob DeBenedetto <deben3@gmail.com>
2020-05-13 22:52:57 -0700

    prevent jumping when touching a ceiling

--- js/app.js
+++ js/app.js
@@ -185,8 +185,25 @@ class App {
         var pairs = e.pairs;
         for (var i = 0; i < pairs.length; i++) {
             var pair = pairs[i];
-            if (pair.bodyA.class == 'player' || pair.bodyA.class) {
-                a.player.allowJump = true;
+            var playerBody = null;
+            var objectBody = null;
+
+            // Check if player is touching object
+            if (pair.bodyA.class == 'player') {
+                playerBody = pair.bodyA;
+                objectBody = pair.bodyB;
+            }
+            else if (pair.bodyB.class == 'player') {
+                playerBody = pair.bodyB;
+                objectBody = pair.bodyA;
+            }
+
+            // Update jump status if playerBody exists in collision check
+            if (playerBody != null) {
+                // Check if player is falling
+                if (a.player.rectangle.velocity.y >= 0) {
+                    a.player.allowJump = true;
+                }
             }
         }
     }
