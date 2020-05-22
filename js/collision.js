49ded237bb0b2e260cafe22893793f3e5bfe9b21
Jacob DeBenedetto <deben3@gmail.com>
2020-05-22 12:40:44 -0700

    update collision behavior

--- js/collision.js
+++ js/collision.js
@@ -7,53 +7,45 @@ class Collision {
         var pairs = e.pairs;
         for (var i = 0; i < pairs.length; i++) {
             var pair = pairs[i];
-            var playerPart = null;
-            var objectPart = null;
-            var player = null;
-            var object = null;
+            var parts = [ pair.bodyA, pair.bodyB ];
 
-            // Check if player is touching object
-            if (pair.bodyA.parent.class == 'player') {
-                playerPart = pair.bodyA;
-                objectPart = pair.bodyB;
-            }
-            else if (pair.bodyB.parent.class == 'player') {
-                playerPart = pair.bodyB;
-                objectPart = pair.bodyA;
-            }
-
-            // Update jump status if playerPart exists in collision check
-            if (playerPart != null) {
-                // Set body objects
-                player = a.level.getObjectByName(playerPart.parent.name);
-                object = a.level.getObjectByName(objectPart.parent.name);
-
+            for (var j = 0; j < parts.length; j++) {
+                var partA = parts[(j + 0) % 2];
+                var partB = parts[(j + 1) % 2];
+                var objectA = a.level.getObjectByName(partA.parent.name);
+                var objectB = a.level.getObjectByName(partB.parent.name);
+                
                 // Check if player is falling
-                if (a.player.body.velocity.y >= 0) { a.player.allowJump = true; }
-
-                // Check spike collision
-                if (objectPart.class == 'sensor') {
-                    if (object.body.class == 'tip') {
+                if (objectA.body.class == 'player') {
+                    if (a.player.body.velocity.y >= 0) { 
+                        a.player.allowJump = true; 
+                    }
+                }
+                
+                if (partA.class == 'sensor') {
+                    if (objectA.body.class == 'tip') {
                         console.log('tip');
                     }
-                    else if (object.body.class == 'bounce') {
-                        var angle = object.body.angle;
-                        var force = object.scale.y / 2; // Use bounce height
-                        player.force(force, angle);
+                    else if (objectA.body.class == 'bounce') {
+                        var angle = objectA.body.angle;
+                        var force = objectA.scale.y / 2; // Use bounce height
+                        if (objectB.body.isStatic == false) objectB.force(force, angle);
+                        if (objectA.body.isStatic == false) objectA.force(-force, angle); // Newton's Third Law
+                        
                     }
-                    else if (object.body.class == 'checkpoint') {
+                    else if (objectA.body.class == 'checkpoint') {
                         console.log('player checkpoint');
                     }
-                    else if (object.body.class == 'spike') {
+                    else if (objectA.body.class == 'spike') {
                         console.log('kill player');
                     }
-                    else if (object.body.class == 'shrink') {
+                    else if (objectA.body.class == 'shrink') {
                         console.log('shrink player');
                     }
-                    else if (object.body.class == 'grow') {
+                    else if (objectA.body.class == 'grow') {
                         console.log('grow player');
                     }
-                    else if (object.body.class == 'finish') {
+                    else if (objectA.body.class == 'finish') {
                         console.log('finish level');
                     }
                 }
