8c3744613260cf026d3bba8384b55f97c1090845
Jacob DeBenedetto <deben3@gmail.com>
2020-07-13 11:54:28 -0700

    add changelog to distributions

--- gulp-build.js
+++ gulp-build.js
@@ -1,8 +1,12 @@
 var gulp = require('gulp');
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('manifest.json', 'utf8'));
+var version = obj.version;
 gulp.src(
     [
         'manifest.json',
         'index.html',
+        'changelog.txt',
         'audio/**/*',
         'css/**/*',
         'fonts/**/*',
@@ -11,6 +15,6 @@ gulp.src(
     ], { base: './' }
 ).pipe(
     gulp.dest(
-        'build'
+        'build/' + version
     )
 );
--- gulp-dist.js
+++ gulp-dist.js
@@ -2,4 +2,8 @@ var gulp = require('gulp');
 var zip  = require('gulp-zip');
 var fs = require('fs');
 var obj = JSON.parse(fs.readFileSync('manifest.json', 'utf8'));
-gulp.src(['./build/**/*']).pipe(zip('boxel3d-' + obj.version + '.zip')).pipe(gulp.dest('./dist'));
+var version = obj.version;
+
+// Distribute to 1) web, and 2) zip format
+gulp.src(['./build/' + version + '/**/*']).pipe(gulp.dest('./dist/website/' + version + '/'));
+gulp.src(['./build/' + version + '/**/*']).pipe(zip('boxel3d-' + version + '.zip')).pipe(gulp.dest('./dist/chrome/'));
