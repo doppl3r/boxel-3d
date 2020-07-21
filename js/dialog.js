09d40aa080bbd4e9d42e3f47bc863e002c40518b
Jacob DeBenedetto <deben3@gmail.com>
2020-07-21 13:35:51 -0700

    add skin files

---
+++ js/dialog.js
@@ -0,0 +1,55 @@
+class Dialog {
+    constructor() {
+
+    }
+
+    add(options) {
+        var dialog = $('<div class="dialog">');
+        var background = $('<div class="background">');
+        var wrapper = $('<div class="wrapper">');
+        var inputs = $('<div class="inputs">');
+
+        // Add attributes to dialog
+        if (options.attributes != null) {
+            dialog.attr(options.attributes);
+            dialog.addClass('dialog')
+        }
+
+        // Include copy
+        if (options.text != null) wrapper.append('<p>' + options.text + '</p>');
+        
+        // Bind functions
+        if (options.inputs != null) {
+            for (var i = 0; i < options.inputs.length; i++) {
+                var data = options.inputs[i];
+                var input = $('<input>', data.attributes);
+
+                // Add default functionality
+                input[0]._function = function(){};
+                input[0]._parameter = input;
+                input[0]._attributes = data.attributes;
+                if (data.function != null) input[0]._function = data.function;
+                if (data.parameter != null) input[0]._parameter = data.parameter;
+                input.on('click', function() {
+                    var self = $(this)[0];
+                    self._function(self._parameter);
+                    if (self._attributes.type == 'button') app.ui.removeDialog(); // Always close dialog
+                });
+                if (data.label != null) inputs.append('<label>' + data.label + '</label>');
+                inputs.append(input);
+            }
+            wrapper.append(inputs);
+        }
+        
+        // Select last option if the background is clicked
+        background.on('click', function() { dialog.find('input:last-of-type').click(); });
+
+        // Append dialog
+        dialog.append(background);
+        dialog.append(wrapper);
+        dialog.hide().fadeIn(100);
+        $('body').append(dialog);
+        $('body').addClass('has-dialog');
+        dialog.find('input:last-of-type').focus();
+    }
+}
