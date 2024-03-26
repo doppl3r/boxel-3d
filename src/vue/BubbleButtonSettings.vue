9876d2bce2b717308ffaa5fc4d3fee330b439f87
Jacob DeBenedetto <deben3@gmail.com>
2024-03-26 07:42:50 -0700

    Update home UI

---
+++ src/vue/BubbleButtonSettings.vue
@@ -0,0 +1,14 @@
+<script setup>
+  import { ref } from 'vue';
+
+  function openSettings() {
+    // Dispatch new popup from event
+    window.dispatchEvent(new CustomEvent('openSettings'));
+  }
+</script>
+
+<template>
+  <a @click="openSettings" title="Settings">
+    <span class="material-symbols-rounded">settings</span>
+  </a>
+</template>
--- src/vue/BubblePageHome.vue
+++ src/vue/BubblePageHome.vue
@@ -1,5 +1,6 @@
 <script setup>
   import { onMounted } from 'vue';
+  import BubbleButtonSettings from './BubbleButtonSettings.vue';
   
   // Run function after being mounted (visible)
   onMounted(function() {
@@ -8,8 +9,36 @@
 </script>
 
 <template>
-  <div class="content">
-    <h1>BOXEL3D</h1>
-    <p>Now includes free cookies!</p>
+  <div class="page">
+    <div class="background">
+      <img src="/img/svg/background-purple.svg">
+    </div>
+    <div class="nav">
+      <BubbleButtonSettings class="button square right" />
+    </div>
+    <div class="content">
+      <h1>BOXEL3D</h1>
+      <p>Now includes free cookies!</p>
+      <div class="carousel">
+        <div class="item">
+          <img src="/img/svg/button-skins.svg">
+          <p class="text">Skins</p>
+        </div>
+        <div class="item">
+          <img src="/img/svg/button-level-editor.svg">
+          <p class="text">Level Editor</p>
+        </div>
+        <div class="item">
+          <img src="/img/svg/button-play.svg">
+          <p class="text">Play</p>
+        </div>
+      </div>
+    </div>
+    <div class="footer">
+      <a class="button wide right">
+        <span class="material-symbols-rounded blue">sms</span>
+        Write a review
+      </a>
+    </div>
   </div>
 </template>
