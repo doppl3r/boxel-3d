3ab615768b82d4c9c3f0cbedeb13a1f74e925cb2
Jacob DeBenedetto <deben3@gmail.com>
2024-12-09 10:22:44 -0700

    Update cube and sphere models

--- v2/src/js/entities/Sphere.js
+++ v2/src/js/entities/Sphere.js
@@ -8,16 +8,26 @@ import { Entity } from '../core/Entity.js';
 */
 
 class Sphere extends Entity {
-  constructor(options) {
+  constructor(options = {}) {
     // Set options with default values
     options = Object.assign({
-      enabledRotations: { x: false, y: false, z: true },
-      enabledTranslations: { x: true, y: true, z: false },
-      radius: 0.5
+      color: '#ffffff',
+      heightSegments: 32,
+      radius: 0.5,
+      widthSegments: 32,
     }, options);
 
     // Create physical shape
     options.shape = new Ball(options.radius);
+
+    // Initialize default sphere model mesh
+    if (options.model == null) {
+      var geometry = new SphereGeometry(options.radius, options.widthSegments, options.heightSegments);
+      var material = new MeshStandardMaterial({ color: options.color });
+      options.model = new Mesh(geometry, material);
+      options.model.receiveShadow = true;
+      options.model.castShadow = true;
+    }
 
     // Inherit Entity class
     super(options);
@@ -26,12 +36,31 @@ class Sphere extends Entity {
     this.isSphere = true;
     this.type = 'sphere';
 
-    // Update 3D object scale
-    this.object.scale.set(options.radius * 2, options.radius * 2, options.radius * 2);
+    // Add optional model to 3D object
+    this.model = options.model;
 
-    // Add entity event listeners
+    // Bind "this" context to class function (required for event removal)
     this.onSphereAdded = this.onSphereAdded.bind(this);
     this.addEventListener('added', this.onSphereAdded);
+  }
+
+  onSphereAdded(e) {
+    // Bind target "this" context to class function (required for event removal)
+    this.onSphereRemoved = this.onSphereRemoved.bind(this);
+
+    // Add optional model to 3D object
+    if (this.model.isObject3D) this.object.add(this.model);
+    
+    // Add Cube event listeners
+    this.addEventListener('removed', this.onSphereRemoved);
+  }
+
+  onSphereRemoved(e) {
+    // Remove model from 3D object
+    if (this.model.isObject3D) this.object.remove(this.model);
+
+    // Remove entity event listeners
+    this.removeEventListener('removed', this.onSphereRemoved);
   }
 
   setRadius(radius) {
@@ -40,48 +69,11 @@ class Sphere extends Entity {
     // Update collider and 3D object scale
     collider.setRadius(radius);
     this.object.scale.set(radius * 2, radius * 2, radius * 2);
-  }
 
-  setScale(scale) {
-    this.setRadius(scale.x / 2);
-  }
-
-  onSphereAdded(e) {
-    this.onSphereRemoved = this.onSphereRemoved.bind(this);
-    this.addEventListener('removed', this.onSphereRemoved);
-
-    // Add 3D model to 3D object
-    if (this.model == null) this.createModel();
-    this.object.add(this.model);
   }
   
-  onSphereRemoved(e) {
-    this.removeEventListener('added', this.onSphereAdded);
-    this.removeEventListener('removed', this.onSphereRemoved);
-  }
-
-  createModel() {
-    var geometry = new SphereGeometry(0.5, 16, 16);
-    var material = new MeshStandardMaterial({ color: '#ffffff' });
-    this.model = new Mesh(geometry, material);
-    this.model.receiveShadow = true;
-    this.model.castShadow = true;
-  }
-
-  toJSON() {
-    // Extend entity toJSON with model name
-    const json = super.toJSON();
-
-    // Assign cube properties to entity JSON
-    Object.assign(json, {
-      type: this.type
-    });
-
-    // Conditionally store model name
-    if (this.model.name) json.model = { name: this.model.name };
-
-    // Return json object
-    return json;
+  setScale(scale) {
+    this.setRadius(scale.x / 2);
   }
 }
 
