93d7e548adbb4bf3b70cac32b60e9960989cf1b4
Jacob DeBenedetto <deben3@gmail.com>
2025-02-04 16:10:28 -0700

    Move levels to levels directory

--- v1/src/js/App.js
+++ v1/src/js/App.js
@@ -291,7 +291,7 @@ class App {
     var levelExists = false;
 
     // Fetch public folder for level
-    await fetch('../json/' + title + '.json').then((response) => {
+    await fetch('../json/levels/' + title + '.json').then((response) => {
       if (response.ok) {
         return response.json();
       }
