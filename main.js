f84254689773523910126a91af85397217af3bee
Jacob DeBenedetto <deben3@gmail.com>
2025-03-03 22:45:52 -0700

    Update electron listeners

--- main.js
+++ main.js
@@ -9,17 +9,55 @@ import path from 'path';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
+// Initialize state keeper
+const store = new Store();
+
+// Initialize main window outside creation
+let mainWindow;
+
 // Remove hidden menu behaviors (ex: zoom)
 Menu.setApplicationMenu(Menu.buildFromTemplate([]));
 
-function createWindow() {
-  // Initialize state keeper
-  const store = new Store();
+function storeWindowSize() {
+  const [width, height] = mainWindow.getSize();
+  store.set({ width, height });
+}
 
+function storeWindowPosition() {
+  const [x, y] = mainWindow.getPosition();
+  store.set({ x, y });
+}
+
+function storeWindowMaximized() {
+  const maximized = mainWindow.isMaximized();
+  store.set({ maximized });
+}
+
+function toggleFullScreen() {
+  mainWindow.setFullScreen(!store.get('fullscreen'));
+  store.set('fullscreen', !store.get('fullscreen'));
+}
+
+function openDevTools() {
+  mainWindow.webContents.openDevTools();
+}
+
+function loadMods(file = '/Charlieee1/Boxel-3d-Mods/main/Boxel 3d Modding API.user.js') {
+  // TEST: Load mods from URL
+  const url = 'https://raw.githubusercontent.com';
+  file = '/Charlieee1/Boxel-3d-Mods/main/Boxel 3d Modding API.user.js';
+  fetch(url + file)
+    .then(response => response.text())
+    .then(code => {
+      // Execute code
+      mainWindow.webContents.executeJavaScript(code);
+    });
+}
+
+function createWindow() {
   // Create the browser window.
-  const mainWindow = new BrowserWindow({
+  mainWindow = new BrowserWindow({
     autoHideMenuBar: false,
-    fullscreen: false,
     height: store.get('height') || 360,
     icon: './build/png/icon128.png',
     show: true,
@@ -35,51 +73,26 @@ function createWindow() {
     y: store.get('y')
   });
 
-  function storeWindowSize() {
-    const [width, height] = mainWindow.getSize();
-    store.set({ width, height });
-  }
+  // Set fullscreen state from store
+  mainWindow.setFullScreen(store.get('fullscreen'));
 
-  function storeWindowPosition() {
-    const [x, y] = mainWindow.getPosition();
-    store.set({ x, y });
-  }
-
-  function storeWindowMaximized() {
-    const maximized = mainWindow.isMaximized();
-    store.set({ maximized });
-  }
-
-  function toggleFullScreen() {
-    mainWindow.setFullScreen(!store.get('fullscreen'));
-    store.set('fullscreen', !store.get('fullscreen'));
-  }
-
-  // Add event listeners
-  mainWindow.on('resized', storeWindowSize);
-  mainWindow.on('moved', storeWindowPosition);
-  mainWindow.on('maximize', storeWindowMaximized);
-  mainWindow.on('unmaximize', storeWindowMaximized);
-
-  // Add event listener to preload.js (bridged to renderer)
-  ipcMain.on('toggleFullScreen', toggleFullScreen);
-  
-  // Add keyboard shortcuts
-  globalShortcut.register('F11', toggleFullScreen);
-  globalShortcut.register('Escape', function() {
-    mainWindow.setFullScreen(false);
-    store.set('fullscreen', false);
-  });
+  // Maximize/fullscreen window if already set
+  if (store.get('maximized')) mainWindow.maximize();
 
   // Show the game only after it is done loading (requires "show": false above to work properly)
   mainWindow.once('ready-to-show', function() {
-    // Maximize/fullscreen window if already set
-    if (store.get('maximized')) mainWindow.maximize();
-    mainWindow.setFullScreen(store.get('fullscreen'));
-
     // Reset zoom before showing app
     mainWindow.webContents.setZoomFactor(1);
     mainWindow.show();
+
+    // Add event listener to preload.js (bridged to renderer)
+    ipcMain.on('toggleFullScreen', toggleFullScreen);
+
+    // Add event listeners
+    mainWindow.on('resized', storeWindowSize);
+    mainWindow.on('moved', storeWindowPosition);
+    mainWindow.on('maximize', storeWindowMaximized);
+    mainWindow.on('unmaximize', storeWindowMaximized);
   });
 
   // Allow game to send users to default browser
@@ -95,24 +108,21 @@ function createWindow() {
   mainWindow.loadFile('./build/index.html', { query: { "fullscreen": true }});
 }
 
-function loadMods(file = '/Charlieee1/Boxel-3d-Mods/main/Boxel 3d Modding API.user.js') {
-  // TEST: Load mods from URL
-  const url = 'https://raw.githubusercontent.com';
-  file = '/Charlieee1/Boxel-3d-Mods/main/Boxel 3d Modding API.user.js';
-  fetch(url + file)
-    .then(response => response.text())
-    .then(code => {
-      // Execute code
-      mainWindow.webContents.executeJavaScript(code);
-    });
-}
-
 // This method will be called when Electron has finished initialization and is ready to create browser windows. Some APIs can only be used after this event occurs.
 app.whenReady().then(function () {
+  // Add keyboard shortcuts
+  globalShortcut.register('F11', toggleFullScreen);
+  globalShortcut.register('Ctrl+Shift+I', openDevTools);
+  globalShortcut.register('Escape', function() {
+    mainWindow.setFullScreen(false);
+    store.set('fullscreen', false);
+  });
+
+  // Create window
   createWindow();
 
+  // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
   app.on('activate', function () {
-    // On macOS it's common to re-create a window in the app when the dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   });
 });
