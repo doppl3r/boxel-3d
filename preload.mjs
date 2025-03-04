c427f327d4763d80f733cfd6ead2f8a3084d3554
Jacob DeBenedetto <deben3@gmail.com>
2025-03-04 07:35:31 -0700

    Update readme

--- preload.mjs
+++ preload.mjs
@@ -1,5 +1,5 @@
-const { ipcRenderer } = require('electron/renderer')
-const { contextBridge } = require('electron')
+const { ipcRenderer } = require('electron/renderer');
+const { contextBridge } = require('electron');
 
 // All of the Node.js APIs are available in the preload process.
 window.addEventListener('DOMContentLoaded', function () {
