b77ee71b6e220bd97fbdef412020928497b54b9a
Jacob DeBenedetto <deben3@gmail.com>
2025-04-28 18:35:37 -0700

    Add android base files

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.boxel3d.app',
+  appName: 'boxel-3d',
+  webDir: 'build'
+};
+
+export default config;
