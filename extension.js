0f4b911e0d5dd5cc738a9a4713d59b65d1cf3c11
Jacob DeBenedetto <deben3@gmail.com>
2025-05-08 11:14:15 -0700

    Fix old ui controls

--- extension.js
+++ extension.js
@@ -10,22 +10,46 @@ let manifestChrome = readFileSync(manifestChromePath, 'utf8');
 let manifestFirefox = readFileSync(manifestFirefoxPath, 'utf8');
 let version = JSON.parse(packageFile).version;
 
+function updateManifestVersionFromPackage() {
+  // Convert string to object
+  const manifestChromeData = JSON.parse(manifestChrome);
+  const manifestFirefoxData = JSON.parse(manifestFirefox);
+
+  // Assign package version to each manifest
+  manifestChromeData.version = version;
+  manifestFirefoxData.version = version;
+
+  // Convert object back to a string before writing
+  manifestChrome = JSON.stringify(manifestChromeData, null, 2);
+  manifestFirefox = JSON.stringify(manifestFirefoxData, null, 2);
+  writeFileSync(manifestChromePath, manifestChrome);
+  writeFileSync(manifestFirefoxPath, manifestFirefox);
+}
+
 function zipBuildFiles(platform = 'chrome', callback = () => {}) {
   // Build Chrome file
   gulp
     .src(['./build/**/*'], { encoding: false })
     .pipe(zip(`boxel-3d-${ version }-${ platform }.zip`, { buffer: true }))
     .pipe(gulp.dest('./dist'))
-    .on('end', callback);
+    .on('end', callback); // Execute callback
 }
 
 function zipSrcFiles() {
-  // Build Chrome file
+  // Create zip file from source files
   gulp
-    .src(['./index.html', './src/**/*', './v1/**/*', './v2/**/*'], { base: '.', encoding: false })
+    .src([
+      './index.html',
+      './src/**/*',
+      './v1/**/*',
+      './v2/**/*'
+    ], { base: '.', encoding: false })
     .pipe(zip(`boxel-3d-${ version }-src.zip`, { buffer: true }))
     .pipe(gulp.dest('./dist'));
 }
+
+// Rewrite manifest versions from package version
+updateManifestVersionFromPackage();
 
 // Start zipping
 zipBuildFiles('chrome', function() {
